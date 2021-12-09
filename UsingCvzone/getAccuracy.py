4512699199a7f84b83821a22b4584d3211bebc3f
jhy979 <jhy979@gmail.com>
2021-12-09 13:10:24 +0900

    feat: getAccuracy 모듈 생성

---
+++ UsingCvzone/getAccuracy.py
@@ -0,0 +1,17 @@
+import csv
+ 
+f = open('./HandInfo.csv','r')
+rdr = csv.reader(f)
+
+count = 0
+entire = 0
+for line in rdr:
+  entire += 1
+  if(line[9]==''):
+    break
+  if "palmar" in line[6] and line[9]=="1":
+    count+=1
+  if "dorsal" in line[6] and line[9]=="0":
+    count+=1
+f.close()
+print(count / entire)
